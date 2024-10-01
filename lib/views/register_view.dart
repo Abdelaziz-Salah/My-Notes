80c57381ecce07cd3b3d9b583fe4ec1f8186f671
yousramamdouhrunprof <149511772+yousramamdouhrunprof@users.noreply.github.com>
2024-10-01 15:40:37 +0300

    Add notes service

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'dart:developer';
 
